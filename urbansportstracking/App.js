88cd86082a06f799ae4a1feaa937a92bd9a525a7
frederikhendrix <rikhendrix@live.nl>
2023-05-25 11:26:47 +0200

    Testing with gifted charts

--- urbansportstracking/App.js
+++ urbansportstracking/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {
-  SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
@@ -16,12 +15,25 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import GiftedCharts from './components/GiftedCharts';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import VerticalBar from './components/VerticalBar';
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <SafeAreaView
+      style={{
+        backgroundColor: '#FFFFFF',
+        flex: 1,
+        alignItems: 'center',
+      }}>
+      <ScrollView>
+        <View style={{flexDirection: 'column'}}>
+          <GiftedCharts />
+          <VerticalBar />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
---
+++ urbansportstracking/components/GiftedCharts.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import {ScrollView} from 'react-native';
+import {View, Text} from 'react-native';
+import {BarChart} from 'react-native-gifted-charts';
+
+const GiftedCharts = () => {
+  const barData = [
+    {value: 250, label: 'M'},
+    {value: 500, label: 'T', frontColor: '#177AD5'},
+    {value: 745, label: 'W', frontColor: '#177AD5'},
+    {value: 320, label: 'T'},
+    {value: 600, label: 'F', frontColor: '#177AD5'},
+    {value: 256, label: 'S'},
+    {value: 300, label: 'S'},
+    {value: 250, label: 'M'},
+    {value: 500, label: 'T', frontColor: '#177AD5'},
+    {value: 745, label: 'W', frontColor: '#177AD5'},
+    {value: 320, label: 'T'},
+    {value: 600, label: 'F', frontColor: '#177AD5'},
+    {value: 256, label: 'S'},
+    {value: 300, label: 'S'},
+    {value: 250, label: 'M'},
+    {value: 1500, label: 'T', frontColor: '#177AD5'},
+    {value: 745, label: 'W', frontColor: '#177AD5'},
+    {value: 320, label: 'T'},
+    {value: 600, label: 'F', frontColor: '#177AD5'},
+    {value: 256, label: 'S'},
+    {value: 300, label: 'S'},
+    {value: 250, label: 'M'},
+    {value: 500, label: 'T', frontColor: '#177AD5'},
+    {value: 745, label: 'W', frontColor: '#177AD5'},
+    {value: 320, label: 'T'},
+    {value: 600, label: 'F', frontColor: '#177AD5'},
+    {value: 256, label: 'S'},
+    {value: 300, label: 'S'},
+    {value: 250, label: 'M'},
+    {value: 500, label: 'T', frontColor: '#177AD5'},
+    {value: 745, label: 'W', frontColor: '#177AD5'},
+    {value: 320, label: 'T'},
+    {value: 600, label: 'F', frontColor: '#177AD5'},
+    {value: 256, label: 'S'},
+    {value: 300, label: 'S'},
+    {value: 250, label: 'M'},
+    {value: 500, label: 'T', frontColor: '#177AD5'},
+    {value: 745, label: 'W', frontColor: '#177AD5'},
+    {value: -320, label: 'T'},
+    {value: 600, label: 'F', frontColor: '#177AD5'},
+    {value: 256, label: 'S'},
+    {value: 300, label: 'S'},
+  ];
+  return (
+    <View style={{flex: 1, margin: 20}}>
+      <ScrollView>
+        <BarChart
+          barWidth={22}
+          noOfSections={6}
+          barBorderRadius={4}
+          frontColor="lightgray"
+          data={barData}
+          dashGap={5}
+          spacing={5}
+          maxValue={1600}
+          yAxisThickness={0}
+          xAxisThickness={0}
+          onPress={(item, index) => console.log('item', item)}
+        />
+      </ScrollView>
+    </View>
+  );
+};
+
+export default GiftedCharts;
